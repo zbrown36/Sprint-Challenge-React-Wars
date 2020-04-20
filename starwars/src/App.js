fd095af60fcfc0c924a551cf26411dc7140f0468
Zach Brown <54756689+zbrown36@users.noreply.github.com>
2020-04-19 22:39:25 -0400

    created base structure need add axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import './App.css';
+import axios from"axios";
+import CharacterCard from "./components/Character";
 
 const App = () => {
+  const [data, setData] = useState([{}]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/")
+    .then(function (response) {
+      console.log(response.data.results);
+      setData(response.data.results)
+    })
+  }, ["https://swapi.py4e.com/api/people/"]);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="header">Star Wars Characters</h1>
+      {data.map(function(item, index) {
+        console.log(CharacterCard)
+        return <CharacterCard name={item.name} height={item.height} weight={item.mass} homeworld={item.homeworld} key={index} />
+      })}
     </div>
-  );
-}
+  );  
+  }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,46 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+background: black;
+color: white;
+width: 700px;
+max-height: 300px;
+display: flex;
+flex: column;
+align-items: center;
+justidy-content: space-between;
+Margin-bottom: 20px;
+`;
+
+const Title = styled.h1`
+font-size = 30px;
+font-color = red;
+border: 1px solid white;
+padding: 5px;
+`;
+const Data = styled.div`
+display:flex;
+width:100%;
+flex-direction:column;
+ali:flex-start;
+border-radius:8px;
+`;
+
+const CharacterCard = props => {
+    return (
+        <Card>
+            <Title>Name: {props.name}</Title>
+            
+            <Data>
+            <h2>Race: {props.race}</h2>
+            <h2>Height: {props.height}</h2>
+            <h2>Weight: {props.weight}</h2>
+            <h2>Home World: {props.homeworld}</h2>
+            </Data>
+        </Card>
+    );
+};
+
+export default CharacterCard;
